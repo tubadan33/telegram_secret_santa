703e4277c4ed91585437db413caffc8d917cbd15
tubadan33 <daniel.herrera33@proton.me>
2024-09-28 21:45:28 -0600

    added halloween strings and default

--- bot/constants/GameStrings.py
+++ bot/constants/GameStrings.py
@@ -1,3 +1,11 @@
+DefaultGameStrings = {
+    "Hitler": "Hitler",
+    "Liberals": "Liberals",
+    "Liberal": "Liberal",
+    "Fascists": "Fascists",
+    "Fascist": "Fascist",
+}
+
 ChirstmasGameStrings = {
     "Hitler": "Santa",
     "Liberals": "Niceists",
@@ -8,8 +16,8 @@ ChirstmasGameStrings = {
 
 HalloweeenGameStrings = {
     "Hitler": "Dracula",
-    "Liberals": "Ghouls",
-    "Liberal": "Ghoul",
-    "Fascists": "Vampires",
-    "Fascist": "Vampire",
+    "Liberals": "Treats",
+    "Liberal": "Treat",
+    "Fascists": "Tricks",
+    "Fascist": "Trick",
 }
